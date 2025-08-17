512a0f08f4eccdf23a11a7a38f2a06b6cc6923ea
Enderson Marques <endersonmqsilva@gmail.com>
2025-08-17 12:49:37 -0300

    criando função para listar amigos

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ function adicionarAmigo() {
     if (amigo !== "") {
         amigos.push(amigo);
         document.getElementById("amigo").value = "";
+        listaDeAmigos();
     } else {
         alert("Por favor, insira um nome.");
     }
 }
 
+function listaDeAmigos() {
+    let lista = document.getElementById("listaAmigos");
+     lista.innerHTML = "";
+     for (let x = 0; x < amigos.length; x++) {
+         let itemLista = document.createElement("li");
+         itemLista.textContent = amigos[x];
+         lista.appendChild(itemLista);
+     }
+}
